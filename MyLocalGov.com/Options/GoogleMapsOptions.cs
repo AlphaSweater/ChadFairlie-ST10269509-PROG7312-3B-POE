8ebf96cef83d4542f610282d1e88f6fa4504e4ed
Chad Fairlie <chad.fairlie2004@gmail.com>
2025-09-04 16:50:14 +0200

    Refactor Maps and GoogleMapsOptions classes
    Removed several classes related to autocomplete requests, predictions, and address validation from `Maps.cs`. Updated `AddressValidationShapedResponse` to simplify its properties and removed `JsonPropertyName` attributes. In `GoogleMapsOptions.cs`, eliminated properties for API key, region code, and language code to streamline configuration options.

--- MyLocalGov.com/Options/GoogleMapsOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyLocalGov.com.Options;
-
-public sealed class GoogleMapsOptions
-{
-	public string ApiKey { get; set; } = string.Empty;
-	public string? RegionCode { get; set; }
-	public string? LanguageCode { get; set; }
-}
